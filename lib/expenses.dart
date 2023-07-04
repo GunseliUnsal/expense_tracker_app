5faa1b974941864628a0e3eea8a13d2d7ec90713
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-04 12:11:50 +0300

    Revert "Added Expense model with UUID integration"
    This reverts commit 4378fc83252e6ea2e8156fabac3887aed2e83ca8.

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -10,13 +10,6 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text("The chart"),
-          Text("Expenses list....")
-        ],
-      )
-    );
+    return Container();
   }
 }
