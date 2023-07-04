4378fc83252e6ea2e8156fabac3887aed2e83ca8
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-04 12:09:42 +0300

    Added Expense model with UUID integration

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -10,6 +10,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text("The chart"),
+          Text("Expenses list....")
+        ],
+      )
+    );
   }
 }
