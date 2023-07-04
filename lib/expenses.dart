e1c632204e5f66eafcf53c965e93302e1718a0b3
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-04 11:36:55 +0300

    Created main.dart and expenses.dart pages
    Added main.dart and expenses.dart pages with their base structure

---
+++ lib/expenses.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
