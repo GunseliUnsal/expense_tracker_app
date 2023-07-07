e594cab7dc53d1da4bbabf5a8f1e54fe4f0d2fbb
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-07 14:53:22 +0300

    added mediaquery

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -62,6 +62,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent =
         const Center(child: Text("No expenses found. Start adding some!"));
 
@@ -82,12 +84,17 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(children: [
+              Expanded(child: Chart(expenses: _registeredExpenses)),
+              Expanded(child: mainContent),
+            ]),
     );
   }
 }
